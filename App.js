b0f390d0f5a00135906efada44835926676e15ee
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2023-11-27 01:15:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1><u>Welcome to Weather Forcast</u></h1>
+      <img src='https://cdn.pixabay.com/photo/2016/03/27/07/32/clouds-1282314_1280.jpg' alt=''></img>
+    </div>
+  );
+}
+
+export default App;
