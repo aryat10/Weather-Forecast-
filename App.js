b73ac342f6a1013e5890bf3363a682e4a83d4018
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2023-11-28 20:07:24 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import './App.css';
-
+import Input from './components/input';
+import Card from './components/card';
+import Button from './components/button';
+import { useWeather } from './context/weather';
 function App() {
+  const weather = useWeather();
+  console.log(weather);
   return (
     <div className="App">
-      <h1><u>Welcome to Weather Forcast</u></h1>
-      <img src='https://cdn.pixabay.com/photo/2016/03/27/07/32/clouds-1282314_1280.jpg' alt=''></img>
+      <h1>Welcome to Weather Forcast</h1>
+      {/* <img src='https://cdn.pixabay.com/photo/2016/03/27/07/32/clouds-1282314_1280.jpg' alt=''></img> */}
+      <Input />
+      <Button onClick = {weather.fetchData} value = 'Search' />
+      <Card />
+      <Button value = 'Refresh' />
     </div>
   );
 }
