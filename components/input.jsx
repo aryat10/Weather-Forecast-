e2650f7925d3864b7003bbf7be9ab2bed0f98aa3
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2023-11-28 20:09:26 +0530

    Update input.jsx

--- components/input.jsx
+++ components/input.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-
-const Input = (props) =>{
+import { useWeather } from "../context/weather";
+const Input = () =>{
+   const weather = useWeather();
      return(
-        <input className="input-field" 
-         placeholder= {props.placeholder}
-         value={props.value}
-         onChange={props.onChange}
+        <input 
+        className="input-field" 
+        placeholder="Search your City's weather"
+         value={weather.searchCity}
+         onChange={(e) => weather.setSearchCity(e.target.value)}
         />
      )
 }
