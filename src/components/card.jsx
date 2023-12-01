bd3e069e44d77e37ff25d5f62ed0abb3aec5ec63
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2023-12-01 11:18:56 +0530

    Update card.jsx

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -7,7 +7,7 @@ const Card = () => {
     return(
         <div className="card">
             <img src= {weather?.data?.current?.condition?.icon} alt=""></img>
-            <h2> {weather.data?.current?.temp_c} ^C </h2>
+            <h2> {weather.data?.current?.temp_c} °C </h2>
         </div>
     )
 }
