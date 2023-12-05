86bb5434e58c2e3c79ffa64dcda0ab59ab8e4f2c
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2023-12-05 14:03:29 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 const baseURL = 'https://api.weatherapi.com/v1/current.json?key=bc114f2e826a41fcb93183552232611';
 
 // This is for city 
+
 export const getWeatherDataForCity = async (city) =>{
     const response = await fetch(`${baseURL}&q=${city}&aqi=yes`);
     return await response.json();
 }
 
 // This is for Lattitude and Longitude of a particular region
-
 
 export const getWeatherDataForLocation = async (lat,lon) =>{
     const response = await fetch(`${baseURL}&q=${lat},${lon}&aqi=yes`);
