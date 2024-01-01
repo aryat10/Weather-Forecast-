2624e43a44f3f98dedf8894909a7388c5517f7eb
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2024-01-01 17:35:36 +0530

    Update App.js

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to Weather Forcast</h1>
-      {/* <img src='https://cdn.pixabay.com/photo/2016/03/27/07/32/clouds-1282314_1280.jpg' alt=''></img> */}
+      
       <Input />
       <Button onClick = {weather.fetchData} value = 'Search' />
       <Card />
